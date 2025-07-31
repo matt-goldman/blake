95346a63526ae6813b6e410b6c8f052b8a325a2b
Matt Goldman <matt.goldman@outlook.com>
2025-07-31 11:03:03 +1000

    Added ILogger to bake pipeline

--- src/Blake.BuildTools/IBlakePlugin.cs
+++ src/Blake.BuildTools/IBlakePlugin.cs
@@ -1,4 +1,6 @@
-﻿namespace Blake.BuildTools;
+﻿using Microsoft.Extensions.Logging;
+
+namespace Blake.BuildTools;
 
 /// <summary>
 /// Defines a plugin interface for extending the behavior of the baking process in a <see cref="BlakeContext"/>.
@@ -12,13 +14,15 @@ public interface IBlakePlugin
     /// Performs any necessary pre-baking operations asynchronously.
     /// </summary>
     /// <param name="context">The context containing information and resources for the baking process.</param>
+    /// <param name="logger">An ILogger instance - default Console logger is passed in by the Blake CLI</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
-    Task BeforeBakeAsync(BlakeContext context) => Task.CompletedTask;
+    Task BeforeBakeAsync(BlakeContext context, ILogger? logger = null) => Task.CompletedTask;
 
     /// <summary>
     /// Performs any necessary operations after the baking process is completed.
     /// </summary>
     /// <param name="context">The context containing information about the completed baking process.</param>
+    /// <param name="logger">An ILogger instance - default Console logger is passed in by the Blake CLI</param>
     /// <returns>A task that represents the asynchronous operation.</returns>
-    Task AfterBakeAsync(BlakeContext context) => Task.CompletedTask;
+    Task AfterBakeAsync(BlakeContext context, ILogger? logger) => Task.CompletedTask;
 }
